061e5750ccd8bd24d160bdcf2f73a53ec79c3189
R Gautham <rgautham3110@gmail.com>
2020-06-04 02:59:28 +0530

    add info component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template >
+  <v-app>
+    <v-img :src="backgroundImage" class="background">
+      <v-app-bar color="#00000000" class="mx-5 mt-5" app flat>
+        <v-toolbar-title id="logo" class>PetStories</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn color="#4D4D4D4D" large dark depressed>LOGIN</v-btn>
+      </v-app-bar>
+      <v-content>
+        <Info />
+      </v-content>
+    </v-img>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Info from "./components/Info";
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
-  }
-}
+    Info
+  },
+
+  data: () => ({
+    backgroundImage:
+      "https://firebasestorage.googleapis.com/v0/b/pet-stories-493e1.appspot.com/o/webpage-assets%2Fbackground.jpg?alt=media&token=6d15684e-ecee-409c-88d1-eb5b269393db"
+  })
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+#logo {
+  font-family: "Pacifico", cursive;
+}
+
+.background {
+  height: 100%;
+  max-height: 100vh;
 }
 </style>
---
+++ src/components/Info.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container class="mt-10">
+    <v-row>
+      <v-col xs="12" md="6">
+        <v-carousel height="300" cycle show-arrows-on-hover>
+          <v-carousel-item v-for="slidePath in carouselImages" :key="slidePath" :src="slidePath"></v-carousel-item>
+        </v-carousel>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col xs="12" md="6">
+        <p class="text-center">We help you find your favorite pet groomers</p>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col xs="12" md="6">
+        <v-row justify="center" no-gutters>
+          <v-icon v-for="(icon, index) in socialIcons" :key="index" class="mx-3">{{icon[0]}}</v-icon>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Info",
+  data: () => ({
+    carouselImages: [
+      "https://firebasestorage.googleapis.com/v0/b/pet-stories-493e1.appspot.com/o/webpage-assets%2Fcarousel-image-1.jpg?alt=media&token=ab627c5d-1bfd-45da-8769-2b8e8539f4d2",
+      "https://firebasestorage.googleapis.com/v0/b/pet-stories-493e1.appspot.com/o/webpage-assets%2Fcarousel-image-2.jpg?alt=media&token=5ec5d36d-959e-4452-ae61-498c87e58d9d",
+      "https://firebasestorage.googleapis.com/v0/b/pet-stories-493e1.appspot.com/o/webpage-assets%2Fcarousel-image-3.jpg?alt=media&token=510a30fc-d02c-4744-8ee6-2fbd8d62968a"
+    ],
+    socialIcons: [["fab fa-facebook"], ["fab fa-twitter"], ["fab fa-instagram"]]
+  })
+};
+</script>
+
+<style scoped>
+</style>
