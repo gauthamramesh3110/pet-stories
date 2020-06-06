746f2560aa95fb4978e80c31d6aa850f55e1ee29
R Gautham <rgautham3110@gmail.com>
2020-06-06 19:22:33 +0530

    Add Signup Form

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template >
   <v-app id="home">
-    <AppBar />
-    <v-content>
-      <Info
-        data-aos="fade"
-        data-aos-delay="0"
-        data-aos-duration="200"
-        data-aos-easing="ease-in-out"
-        data-aos-mirror="true"
-        data-aos-once="false"
-        data-aos-anchor-placement="top-center"
-      />
-      <List />
-    </v-content>
+    <v-img class="overflow-y-auto" :src="backgroundImage" contain position="top">
+      <AppBar />
+      <v-content>
+        <Info
+          data-aos="fade"
+          data-aos-delay="0"
+          data-aos-duration="200"
+          data-aos-easing="ease-in-out"
+          data-aos-mirror="true"
+          data-aos-once="false"
+          data-aos-anchor-placement="top-center"
+        />
+        <List />
+      </v-content>
+    </v-img>
   </v-app>
 </template>
 
 <script>
-import AppBar from "./components/AppBar"
+import AppBar from "./components/AppBar";
 import Info from "./components/Info";
 import List from "./components/List";
 import AOS from "aos";
@@ -34,7 +36,7 @@ export default {
 
   data: () => ({
     backgroundImage:
-      "https://firebasestorage.googleapis.com/v0/b/pet-stories-493e1.appspot.com/o/webpage-assets%2Fbackground.jpg?alt=media&token=6d15684e-ecee-409c-88d1-eb5b269393db"
+      "https://firebasestorage.googleapis.com/v0/b/pet-stories-493e1.appspot.com/o/webpage-assets%2Fcat-background.png?alt=media&token=71fcaf39-260f-45d0-89bc-192becd29aca"
   }),
 
   methods: {
@@ -54,7 +56,7 @@ export default {
 
 <style scoped>
 #home {
-  background: url("https://firebasestorage.googleapis.com/v0/b/pet-stories-493e1.appspot.com/o/webpage-assets%2Fbackground.jpg?alt=media&token=6d15684e-ecee-409c-88d1-eb5b269393db");
+  background: black;
   background-size: contain;
 }
 </style>
--- src/components/LoginDialog.vue
+++ src/components/LoginDialog.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog width="400">
+  <v-dialog :width="window==0?'450':'900'">
     <template v-slot:activator="{ on }">
       <v-btn color="#4D4D4D4D" large dark depressed v-on="on">LOGIN</v-btn>
     </template>
@@ -26,7 +26,41 @@
           </div>
         </v-window-item>
         <v-window-item>
-            Register
+          <div class="d-flex flex-column py-6 px-3">
+            <v-form>
+              <v-container>
+                <v-row>
+                  <v-col md="6" cols="12">
+                    <div class="d-flex">
+                      <v-text-field label="First Name" filled class="mr-3"></v-text-field>
+                      <v-text-field label="Last Name" filled></v-text-field>
+                    </div>
+                    <v-text-field label="Email" filled type="email"></v-text-field>
+                    <v-text-field label="Username" hint="Choose a unique username" filled></v-text-field>
+                    <div class="d-flex">
+                      <v-text-field label="Password" filled class="mr-3" type="password"></v-text-field>
+                      <v-text-field label="Retype Password" filled type="password"></v-text-field>
+                    </div>
+                  </v-col>
+                  <v-col cols="12" md="6">
+                    <v-text-field label="Address Field 1" filled></v-text-field>
+                    <v-text-field label="Address Field 2" filled></v-text-field>
+                    <div class="d-flex">
+                      <v-select class="mr-3" label="State" :items="cities" filled></v-select>
+                      <v-select label="City" :items="cities" filled></v-select>
+                    </div>
+                    <div class="d-flex">
+                      <v-text-field label="Pin Code" filled class="mr-3"></v-text-field>
+                      <v-text-field label="Phone Number" filled></v-text-field>
+                    </div>
+                  </v-col>
+                </v-row>
+                <v-row justify="center">
+                  <v-btn color="secondary" dark>Regsiter</v-btn>
+                </v-row>
+              </v-container>
+            </v-form>
+          </div>
         </v-window-item>
       </v-window>
     </v-card>
@@ -36,10 +70,11 @@
 <script>
 export default {
   name: "LoginDialog",
-  data(){
-      return {
-          window: 0
-      }
+  data() {
+    return {
+      window: 0,
+      cities: ["Tamil Nadu", "Maharashtra", "West Bengal"]
+    };
   }
 };
 </script>
